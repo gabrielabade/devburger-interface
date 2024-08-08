4bde7867882271999533340455866ad97503b15d
gabrielabade <gabriela.bade@hotmail.com>
2024-08-08 15:17:29 -0300

    Global Styles with Styled-Components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+
 import { Login } from './containers/Login/index.jsx';
+import GlobalStyles from './styles/globalStyles.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Login />
+    <GlobalStyles />
   </React.StrictMode>,
 );

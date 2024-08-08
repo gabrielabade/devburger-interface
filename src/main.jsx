e134a6532ca29ab29a06fdae372a572000f7f68e
gabrielabade <gabriela.bade@hotmail.com>
2024-08-08 12:24:54 -0300

    configuring ESlint + Prettier in React

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Login } from './containers/Login/index.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-
+import { Login } from './containers/Login/index.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Login />
   </React.StrictMode>,
-)
+);

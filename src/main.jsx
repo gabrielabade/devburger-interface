680e691961a12c2491133c569e07b2df34a87c75
gabrielabade <gabriela.bade@hotmail.com>
2024-08-20 16:49:58 -0300

    Creating the user

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
-import { Login } from './containers/Login/index.jsx';
+import 'react-toastify/dist/ReactToastify.css';
+import { router } from './routes';
 import GlobalStyles from './styles/globalStyles.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login />
+    <RouterProvider router={router} />
     <GlobalStyles />
     <ToastContainer autoClose={2000} theme="light" />
   </React.StrictMode>,

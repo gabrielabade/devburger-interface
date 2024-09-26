c3af0bf4ec401c2c9065564913198da7c33f28e7
gabrielabade <gabriela.bade@hotmail.com>
2024-09-26 16:14:27 -0300

    still creating context

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import { RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
+import AppProvider from './hooks/index.jsx';
 import { router } from './routes';
 import GlobalStyles from './styles/globalStyles.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <GlobalStyles />
-    <ToastContainer autoClose={2000} theme="light" />
+    <AppProvider>
+      <RouterProvider router={router} />
+      <GlobalStyles />
+      <ToastContainer autoClose={2000} theme="light" />
+    </AppProvider>
   </React.StrictMode>,
 );

09fe7c5ccda50f9fe37984ef633c2c4220fa321b
gabrielabade <gabriela.bade@hotmail.com>
2024-09-30 16:13:28 -0300

    initial header

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
+import { Header } from './components/Header/index.jsx';
 import AppProvider from './hooks/index.jsx';
 import { router } from './routes';
 import GlobalStyles from './styles/globalStyles.js';
@@ -11,6 +12,7 @@ import GlobalStyles from './styles/globalStyles.js';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppProvider>
+      <Header />
       <RouterProvider router={router} />
       <GlobalStyles />
       <ToastContainer autoClose={2000} theme="light" />

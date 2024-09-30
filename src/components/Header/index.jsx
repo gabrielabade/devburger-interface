d633bb343514af6057ce9945c7152eac65aa342d
gabrielabade <gabriela.bade@hotmail.com>
2024-09-30 17:02:43 -0300

    styling the Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,44 @@
+import { UserCircle, ShoppingCard } from 'phosphor-icons/react ';
+
+import {
+  Container,
+  HeaderLink,
+  LinkContainer,
+  Navigation,
+  Options,
+  Profile,
+  Logout,
+  Content,
+} from './styles';
+
 export function Header() {
   return (
     <div>
-      <h1>Header</h1>
+      <Container>
+        <Content>
+          <Navigation>
+            <div>
+              <HeaderLink>Home</HeaderLink>
+              <HeaderLink>Cardápio</HeaderLink>
+            </div>
+          </Navigation>
+          <Options>
+            <Profile>
+              <UserCircle color="#fff" size={24} />
+              <div>
+                <p>
+                  Olá, <span>Birubs</span>
+                </p>
+                <Logout>Sair</Logout>
+              </div>
+            </Profile>
+            <LinkContainer>
+              <ShoppingCard color="#fff" size={24} />
+              <HeaderLink>Carrinho</HeaderLink>
+            </LinkContainer>
+          </Options>
+        </Content>
+      </Container>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
-import { Header } from './components/Header/index.jsx';
 import AppProvider from './hooks/index.jsx';
 import { router } from './routes';
 import GlobalStyles from './styles/globalStyles.js';
@@ -12,7 +11,6 @@ import GlobalStyles from './styles/globalStyles.js';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppProvider>
-      <Header />
       <RouterProvider router={router} />
       <GlobalStyles />
       <ToastContainer autoClose={2000} theme="light" />

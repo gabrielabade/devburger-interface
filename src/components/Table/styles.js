b1155ea86c55e972b5596e02fc99c99753a3fc56
gabrielabade <gabriela.bade@hotmail.com>
2024-10-07 16:41:15 -0300

    initial table cart

---
+++ src/components/Table/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Root = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  background-color: white;
+  border-radius: 20px;
+`;
+
+export const Header = styled.thead``;
+
+export const Tr = styled.tr``;
+
+export const Th = styled.th``;
+
+export const Td = styled.td``;
